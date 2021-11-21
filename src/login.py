9cd1bd51546b5fc7589180401131cedb7a9a6fd6
Rahmat Fabhian Aminuddin <18219055@std.stei.itb.ac.id>
2021-11-21 03:25:23 +0000

    Ganti sistem penyimpanan jadi mariadb demi masa depan pengintegrasian yang lebih baik

--- src/login.py
+++ src/login.py
@@ -1,36 +1,39 @@
-from tkinter import *
+import sys
+if "tkinter" not in sys.modules:
+    from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import ttk, messagebox
 import os
+import mysql.connector as mysql
+import mariadb
 
-
-def main_screen():
+def home():
   global screen
   screen = Tk()
   screen.title("MONITROS")
-  screen.geometry("1920x1080")
+  screen.geometry("1270x690")
   screen.config(bg = "white")
   screen.bg_img = ImageTk.PhotoImage(file="img/login.jpg")
   background = Label(screen, image = screen.bg_img).place(x = 0, y = 0, relwidth = 1, relheight = 1)
 
-  global username_verify
-  global password_verify
+  global Username_verify
+  global Passwords_verify
   
-  username_verify = StringVar()
-  password_verify = StringVar()
+  Username_verify = StringVar()
+  Passwords_verify = StringVar()
 
-  global username_login
-  global password_login
+  global Username_login
+  global Passwords_login
   
-  #field username
+  #field Username
   Label(screen, text = "Username", font = ("Helvetica", 15, "bold"), bg="white").place(x = 850, y = 220)
-  username_login = Entry(screen, textvariable = username_verify, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  username_login.place(x = 850, y = 250, width = 300, height = 30)
+  Username_login = Entry(screen, textvariable = Username_verify, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Username_login.place(x = 850, y = 250, width = 300, height = 30)
 
-  #field password
-  Label(screen, text = "Password", font = ("Helvetica", 15, "bold"), bg="white").place(x = 850, y = 320)
-  password_login = Entry(screen, textvariable = password_verify, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  password_login.place(x = 850, y = 350, width = 300, height = 30)
+  #field Passwords
+  Label(screen, text = "Passwords", font = ("Helvetica", 15, "bold"), bg="white").place(x = 850, y = 320)
+  Passwords_login = Entry(screen, textvariable = Passwords_verify, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Passwords_login.place(x = 850, y = 350, width = 300, height = 30)
 
   #button login
   Button(screen, text = "LOGIN", font = ("Helvetica", 15, "bold"), bg="cyan", width = 10, height = 1, command = login_verify).place(x = 940, y = 420)
@@ -45,92 +48,119 @@ def register():
   global screen1
   screen1 = Toplevel(screen)
   screen1.title("MONITROS")
-  screen1.geometry("1920x1080")
+  screen1.geometry("1270x690")
   screen1.config(bg = "white")
   screen1.bg_img = ImageTk.PhotoImage(file="img/register.jpg")
   background = Label(screen1, image = screen1.bg_img).place(x = 0, y = 0, relwidth = 1, relheight = 1)
 
-  global username
-  global password
-  global username_register
-  global password_register
-  username = StringVar()
-  password = StringVar()
+  global Username
+  global Passwords
+  global Nama_lengkap
+  global Nomor_HP
+  global Alamat
+  global Role
 
-  #field username
+  Username = StringVar()
+  Passwords = StringVar()
+  Nama_lengkap = StringVar()
+  Nomor_HP = StringVar()
+  Alamat = StringVar()
+  Role = StringVar()
+
+  global Username_register
+  global Passwords_register
+  global Nama_lengkap_register
+  global Nomor_HP_register
+  global Alamat_register
+  global Role_register
+
+  #field Username
   Label(screen1, text = "Username", font = ("Helvetica", 15, "bold"), bg="white").place(x = 50, y = 200)
-  username_register = Entry(screen1, textvariable = username, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  username_register.place(x = 50, y = 230, width = 300, height = 30)
+  Username_register = Entry(screen1, textvariable = Username, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Username_register.place(x = 50, y = 230, width = 300, height = 30)
 
-  #field password
-  Label(screen1, text = "Password", font = ("Helvetica", 15, "bold"), bg="white").place(x = 450, y = 400)
-  password_register = Entry(screen1, textvariable = password, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  password_register.place(x = 450, y = 430, width = 300, height = 30)
+  #field Passwords
+  Label(screen1, text = "Passwords", font = ("Helvetica", 15, "bold"), bg="white").place(x = 450, y = 400)
+  Passwords_register = Entry(screen1, textvariable = Passwords, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Passwords_register.place(x = 450, y = 430, width = 300, height = 30)
 
   #field nama
   Label(screen1, text = "Nama Lengkap", font = ("Helvetica", 15, "bold"), bg="white").place(x = 50, y = 300)
-  nama_register = Entry(screen1, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  nama_register.place(x = 50, y = 330, width = 300, height = 30)
+  Nama_lengkap_register = Entry(screen1, textvariable = Nama_lengkap, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Nama_lengkap_register.place(x = 50, y = 330, width = 300, height = 30)
 
   #field nomor hp
   Label(screen1, text = "Nomor HP", font = ("Helvetica", 15, "bold"), bg="white").place(x = 50, y = 400)
-  phone_register = Entry(screen1, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  phone_register.place(x = 50, y = 430, width = 300, height = 30)
+  Nomor_HP_register = Entry(screen1, textvariable = Nomor_HP, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Nomor_HP_register.place(x = 50, y = 430, width = 300, height = 30)
 
-  #field alamat
+  #field Alamat
   Label(screen1, text = "Alamat", font = ("Helvetica", 15, "bold"), bg="white").place(x = 450, y = 200)
-  alamat_register = Entry(screen1, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  alamat_register.place(x = 450, y = 230, width = 300, height = 30)
+  Alamat_register = Entry(screen1, textvariable = Alamat, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Alamat_register.place(x = 450, y = 230, width = 300, height = 30)
 
-  #field posisi
+  #field role
   Label(screen1, text = "Role", font = ("Helvetica", 15, "bold"), bg="white").place(x = 450, y = 300)
-  role_register = Entry(screen1, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
-  role_register.place(x = 450, y = 330, width = 300, height = 30)
+  Role_register = Entry(screen1, textvariable = Role, font=("Helvetica", 15, "bold"), bg = "light grey", fg = "black")
+  Role_register.place(x = 450, y = 330, width = 300, height = 30)
 
   #button register
   Button(screen1, text = "DAFTAR", font=("Helvetica", 15, "bold"), bg = "cyan", fg = "black", command = register_user).place(x = 900, y = 315, height = 50, width = 160)
 
 #fungsi actionzzzz
 def register_user():
-  username_info = username.get()
-  password_info = password.get()
+  # Connect to Mariadb
+  try:
+    conn = mariadb.connect(
+        user="root",
+        password="",
+        host="localhost",
+        port=3306,
+        database="monitros"
+    )
+  except mariadb.Error as e:
+      print(f"Error connecting to MariaDB Platform: {e}")
+      sys.exit(1)
+  
+  # Get Cursor
+  cur = conn.cursor()
+  Query = "INSERT INTO data_user VALUES (%s,%s,%s,%d,%s,%s)"
+  data = (Username.get(),Passwords.get(),Nama_lengkap.get(),Nomor_HP.get(),Alamat.get(),Role.get())
+  cur.execute(Query, data)
+  conn.commit()
 
-  file = open(username_info, "w")
-  file.write(username_info+"\n")
-  file.write(password_info)
-  file.close()
-
-  username_register.delete(0, END)
-  password_register.delete(0, END)
-
-  Label(screen1, text = "Berhasil terdaftar!", fg = "green" ,font = ("Helveica", 11)).pack()
+  Label(screen1, text = "Berhasil terdaftar!", fg = "green" ,font = ("Helvetica", 11)).pack()
 
 def login_verify():
-  username1 = username_verify.get()
-  password1 = password_verify.get()
-  username_login.delete(0, END)
-  password_login.delete(0, END)
+  # Connect to Mariadb
+  try:
+    conn = mariadb.connect(
+        user="root",
+        password="",
+        host="localhost",
+        port=3306,
+        database="monitros"
+    )
+  except mariadb.Error as e:
+      print(f"Error connecting to MariaDB Platform: {e}")
+      sys.exit(1)
 
-  list_of_files = os.listdir()
-  if username1 in list_of_files:
-    file1 = open(username1, "r")
-    verify = file1.read().splitlines()
-    if password1 in verify:
-        berhasil_login()
-    else:
-        password_salah()
-
+  # Get Cursor
+  cur = conn.cursor()
+  cur.execute("select * from data_user where Username=%s and Password=%s",(Username_login.get(),Passwords_login.get()))
+  row=cur.fetchone()
+  if row == None:
+      pengguna_salah()
   else:
-        pengguna_salah()
+      berhasil_login()
+  conn.commit()
 
 #the ifs
 def berhasil_login():
   Label(screen, text = "Berhasil login!", fg = "green" ,font = ("Helvetica", 13)).pack()
 
-def password_salah():
-  Label(screen, text = "Password yang anda masukkan salah!", fg = "green" ,font = ("Helvetica", 13)).pack()
-
 def pengguna_salah():
-  Label(screen, text = "Pengguna tidak ditemukan!", fg = "green" ,font = ("Helvetica", 13)).pack()
+  Label(screen, text = "Username atau Passwords salah! Coba lagi", fg = "green" ,font = ("Helvetica", 13)).pack()
         
-main_screen()
+#gas GUI        
+home()
