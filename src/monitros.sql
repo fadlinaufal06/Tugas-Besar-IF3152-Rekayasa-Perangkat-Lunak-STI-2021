ec2938a015195050403625bfe2a8b0e5b5c512bc
deanarsp <18219034@std.stei.itb.ac.id>
2021-11-24 08:03:50 +0700

    add todolist table to database

--- src/monitros.sql
+++ src/monitros.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: monitros
 -- ------------------------------------------------------
--- Server version	10.4.21-MariaDB
+-- Server version	10.6.5-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,8 +29,9 @@ CREATE TABLE `data_user` (
   `Nomor_HP` int(15) NOT NULL,
   `Alamat` varchar(25) NOT NULL,
   `Role` varchar(10) NOT NULL,
-  PRIMARY KEY (`Username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`Username`),
+  KEY `role_idx` (`Role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,7 +56,7 @@ CREATE TABLE `menu` (
   `Nama_Menu` varchar(50) NOT NULL,
   `Deskripsi` varchar(100) NOT NULL,
   PRIMARY KEY (`IDMenu`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +84,7 @@ CREATE TABLE `peralatan` (
   `Jumlah_Peralatan` int(11) NOT NULL,
   `Kondisi` varchar(25) NOT NULL,
   PRIMARY KEY (`IDPeralatan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,6 +96,38 @@ LOCK TABLES `peralatan` WRITE;
 INSERT INTO `peralatan` VALUES (1,'Piring','AlatMakan',200,170,'Butuh Beli Baru'),(2,'sdasd','dasdasd',12,11,'sadsad'),(3,'Garpu','AlatMakan',100,70,'Butuh Beli Baru'),(4,'Sumpit','AlatMakan',100,90,'Aman'),(5,'MangkokM','AlatMakan',150,145,'Aman'),(6,'Burner','AlatMeja',15,15,'Aman'),(7,'Tong','AlatMakan',30,30,'Aman'),(8,'Gunting','AlatMakan',15,15,'Aman'),(9,'Gelas','AlatMakan',80,75,'Aman'),(10,'Pitcher','AlatWaiter',10,9,'Aman'),(11,'Sapu','AlatKebersihan',10,10,'Aman'),(12,'Pel','AlatKebersihan',10,10,'Aman'),(13,'Kain Lap','AlatKebersihan',20,14,'Butuh Beli Baru'),(14,'Kotak Tissue','AlatMeja',45,20,'Butuh Beli Baru'),(15,'Condament','AlatMeja',30,29,'Aman'),(16,'Tong Sampah','AlatMeja',6,6,'Aman'),(17,'Tray','AlatWaiter',8,8,'Aman');
 /*!40000 ALTER TABLE `peralatan` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `todolist`
+--
+
+DROP TABLE IF EXISTS `todolist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `todolist` (
+  `IDPekerjaan` int(11) NOT NULL,
+  `Username` varchar(15) NOT NULL,
+  `Role` varchar(10) NOT NULL,
+  `Nama_Pekerjaan` varchar(50) NOT NULL,
+  `Deskripsi` varchar(50) NOT NULL,
+  `Status` varchar(25) NOT NULL,
+  PRIMARY KEY (`IDPekerjaan`),
+  KEY `fk_usr` (`Username`),
+  KEY `fk_role` (`Role`),
+  CONSTRAINT `fk_role` FOREIGN KEY (`Role`) REFERENCES `data_user` (`Role`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_usr` FOREIGN KEY (`Username`) REFERENCES `data_user` (`Username`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todolist`
+--
+
+LOCK TABLES `todolist` WRITE;
+/*!40000 ALTER TABLE `todolist` DISABLE KEYS */;
+INSERT INTO `todolist` VALUES (1,'arga34','Karyawan','Rekap Penjualan','Mengecek ulang dan merekap hasil penjualan','Belum Dilakukan'),(2,'brunofernandes','Karyawan','Update stok','Hitung dan validasi stok di gudang','Sudah dilakukan');
+/*!40000 ALTER TABLE `todolist` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -105,4 +138,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-22  9:52:48
+-- Dump completed on 2021-11-24  8:03:00
