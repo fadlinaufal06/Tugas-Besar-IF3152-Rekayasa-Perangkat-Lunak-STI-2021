4f808683820194c051abeafc41af15210f3bd9ea
rfa435 <68505238+rfa435@users.noreply.github.com>
2021-12-01 14:44:52 +0700

    fix pytest login

--- test/test_login.py
+++ test/test_login.py
@@ -25,10 +25,10 @@ def Connect():
 
     # Get Cursor
     cur = conn.cursor()
-    cur.execute("select * from data_user where Username=%s and Password=%s")
+    cur.execute("select * from data_user")
     return cur.fetchall()
 
-def test_registrasi(query,data):
+def registrasilogin(query,data):
     conn = mariadb.connect(
             user="root",
             password="",
@@ -42,5 +42,31 @@ def test_registrasi(query,data):
         cur.execute(query, data)
         conn.commit()
         return 'berhasil'
+    except:
+        return 'tidak berhasil'
+
+def test_connnection():
+    regtest = Connect()
+    assert (regtest[0] == ('abhifabhian','abhi','abhi fabhian',213123,'Tangerang','Owner'))
+
+def test_registrasi():
+    query = "INSERT INTO data_user VALUES (%s,%s,%s,%d,%s,%s)"
+    data = ('sugino','sss123','sugino fabhian',22123,'Jakarta','Owner')
+    message = registrasilogin(query, data)
+    assert (message == 'berhasil')
+
+def test_login():
+    conn = mariadb.connect(
+            user="root",
+            password="",
+            host="localhost",
+            port=3306,
+            database="monitros"
+        )
+    # Get Cursor
+    try:
+        cur = conn.cursor()
+        message = cur.execute("SELECT * FROM data_user WHERE Username=%s and Password=%s",('abhifabhian','abhi'))
+        assert (message == 'berhasil')
     except:
         return 'tidak berhasil'
